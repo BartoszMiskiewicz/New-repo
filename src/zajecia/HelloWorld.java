1141fe1665e28d7612b819a2c0ef7212bcc555f6
unknown <bartosz-miskiewicz@wp.pl>
2017-04-06 20:44:11 +0200

    initial commit

---
+++ src/zajecia/HelloWorld.java
@@ -0,0 +1,10 @@
+package zajecia;
+
+/**
+ * Created by Bartosz on 2017-04-06. skrot do main - psvm
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
